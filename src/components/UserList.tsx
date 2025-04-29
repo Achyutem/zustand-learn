3d407e2eb7a3dfff04f0f72e742110f4a0be8b1a
achyutem <achyutem.ad@gmail.com>
2025-04-29 23:37:03 +0530

    add toggle for admin only

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useUserStore } from "../useUserStore";
 
 export const UserList: React.FC = React.memo(() => {
 	const { users, selectedUserId, selectUser } = useUserStore();
+	const [showAdminsOnly, setShowAdminsOnly] = useState(false);
+
+	const showFilteredUsers = showAdminsOnly
+		? users.filter((user) => user.isAdmin)
+		: users;
+
 	return (
 		<div>
+			<button onClick={() => setShowAdminsOnly((prev) => !prev)}>
+				{showAdminsOnly ? "show all users" : "show admin only"}
+			</button>
 			<h2>Users</h2>
 			<ul>
-				{users.map((user) => (
+				{showFilteredUsers.map((user) => (
 					<li
 						key={user.id}
 						style={{
