145fa9adbe6fc26ed22cdd9650a6b990a79775c1
achyutem <achyutem.ad@gmail.com>
2025-05-01 16:41:31 +0530

    upped the state to context store

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { UserList } from "./components/UserList";
 import { UserProfile } from "./components/UserProfile";
 import { Moon, Sun } from "lucide-react";
+import { useUserStore } from "./useUserStore";
 
 export const App: React.FC = () => {
-	const [isDarkMode, setIsDarkMode] = useState(false);
+	const { isDarkMode, setDarkMode, showAdminsOnly, setShowAdminsOnly } =
+		useUserStore();
 
 	useEffect(() => {
 		document.body.classList.toggle("dark-mode", isDarkMode);
 	}, [isDarkMode]);
 
 	const toggleDarkMode = () => {
-		setIsDarkMode(!isDarkMode);
+		setDarkMode(!isDarkMode);
 	};
 
 	return (
 		<div className="main">
 			<div className="header centre">
 				<h1>Profile Dashboard</h1>
-				<button onClick={toggleDarkMode}>
-					{isDarkMode ? <Sun /> : <Moon />}
-				</button>
+				<div className="button-group">
+					<button
+						className="admin-button"
+						onClick={() => setShowAdminsOnly(!showAdminsOnly)}
+					>
+						{showAdminsOnly ? "all users" : "admin only"}
+					</button>
+					<button className="theme-button" onClick={toggleDarkMode}>
+						{isDarkMode ? <Sun /> : <Moon />}
+					</button>
+				</div>
 			</div>
 			<UserList />
 			<UserProfile />
